75be20fe09e525000f03a22228426abc770caf16
Eduardo Silva <eduardo.ejs@hotmail.com>
2020-03-04 09:17:16 -0300

    Ajustes no layout de leitura de noticias - cards de arquivos e imagens

--- website/framework/portal/resources/views/site/publico/noticias/ler_noticia.blade.php
+++ website/framework/portal/resources/views/site/publico/noticias/ler_noticia.blade.php
@@ -2,27 +2,32 @@
 
 @section('conteudo')
     <div class="container">
-        @if (isset($noticia))
-            <div class="jumbotron mt-2">
+        @if (isset($noticia))                        
+            <img class="float-right ml-2 img-thumbnail" src="{{ url('storage/imagens/noticias/'.$noticia->id.'/thumbnail/medium/'. $noticia->imagens()->where('ordem',1)->first()->nome_arquivo) }}" alt="news">
                 <h1>{{$noticia->titulo}}</h1>
                 <small class="text-muted">Publicado em: {{ \Carbon\Carbon::parse($noticia->created_at)->format('d/m/Y H:i') }}</small>
+            <div class="text-justify mt-3" id="corpo">                    
+                {!! $noticia->conteudo !!}
             </div>
-            <div class="media">                
-                <div class="media-body text-justify" id="corpo">
-                    <img src="{{ url('storage/imagens/noticias/'.$noticia->id.'/thumbnail/medium/'. $noticia->imagens()->where('ordem',1)->first()->nome_arquivo) }}" alt="news" class="">
-                    {!! $noticia->conteudo !!}
-                </div>
-            </div>            
+            
             @card_arquivos_component(['model' => $noticia, 'url' => 'noticia'])
             @endcard_arquivos_component
-                       
-            <div >
-                @foreach ($noticia->imagens as $item)
-                    <a class="example-image-link" href="{{ url('storage/imagens/noticias/'.$noticia->id.'/'. $item->nome_arquivo) }}" data-lightbox="roadtrip">
-                        <img class="example-image" src="{{ url('storage/imagens/noticias/'.$noticia->id.'/thumbnail/small/'. $item->nome_arquivo) }}">
-                    </a>                    
-                @endforeach                
-            </div>
+              
+            @if (count($noticia->imagens) > 1)
+                <div class="card border-dark mb-3">
+                    <div class="card-header">
+                        <i class="far fa-images"></i> Galeria de imagens
+                    </div>
+                    <div class="card-body text-center">
+                        @foreach ($noticia->imagens as $item)
+                            <a class="example-image-link" href="{{ url('storage/imagens/noticias/'.$noticia->id.'/'. $item->nome_arquivo) }}" data-lightbox="roadtrip">
+                                <img class="example-image" src="{{ url('storage/imagens/noticias/'.$noticia->id.'/thumbnail/small/'. $item->nome_arquivo) }}">
+                            </a>                    
+                        @endforeach            
+                    </div>    
+                </div>    
+            @endif
+            
         @endif
     </div>    
 @endsection
