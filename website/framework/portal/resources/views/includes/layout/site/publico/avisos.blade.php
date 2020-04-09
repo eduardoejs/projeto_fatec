eddf1ee66496bf72fa3ae1fce1904d93cb4aecab
Eduardo Silva <eduardo.ejs@hotmail.com>
2020-04-09 12:57:12 -0300

    Alterando card de avisos

--- website/framework/portal/resources/views/includes/layout/site/publico/avisos.blade.php
+++ website/framework/portal/resources/views/includes/layout/site/publico/avisos.blade.php
@@ -1,10 +1,12 @@
 @if ($avisos)
-    <!-- Avisos Importantes -->
+<!-- Avisos Importantes -->
+<div class="jumbotron jumbotron-fluid bg-light">
     <div class="container">
         <div class="row justify-content-sm-center mt-2">
             @card_aviso_component(['avisos' => $avisos ?? null])
             @endcard_aviso_component
         </div>
     </div>
-    <!-- End Avisos Importantes -->    
+</div>
+<!-- End Avisos Importantes -->    
 @endif
